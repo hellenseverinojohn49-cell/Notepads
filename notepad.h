ab21379f0bb58d4e7c84461eaddbb9a3bfa1d2a9
hellenseverinojohn49-cell <hellenseverinojohn49@gmail.com>
2026-06-02 18:32:25 +0300

    Add files via upload

---
+++ notepad.h
@@ -0,0 +1,13 @@
+#ifndef NOTEPAD_H
+#define NOTEPAD_H
+
+#include <string>
+
+using namespace std;
+
+// Function prototypes
+void writeFile(string filename);
+void readFile(string filename);
+void appendFile(string filename);
+
+#endif
